6c3d139b0b48133f7ecc8afd191d8a8c0e4403ef
Chris Buckley <chris@cmbuckley.co.uk>
2013-11-12 11:02:26 +0000

    rm old map text

--- maps/88-queen-victoria-st/index.php
+++ maps/88-queen-victoria-st/index.php
@@ -1,28 +1,3 @@
 <?php
-	header('Location: http://maps.google.co.uk/maps?f=q&hl=en&q=53.945294,-1.092094+(88+Queen+Victoria+St)&ie=UTF8&om=1&ll=53.945295,-1.092099&spn=0.004098,0.010042&z=17&iwloc=addr');
-	exit;
 
-	require('../../.include/header.php');
-?>
-				<h2>88 Queen Victoria Street</h2>
-				<h3>4-Bedroom Terraced House</h3>
-
-				<p id="desc">Located in the South Bank area of York, this property is really close to the College of Law and has easy access to the University of York via the Millennium Bridge by foot or bike.</p>
-
-				<p id="price"><strong>&pound;55&ndash;60 per room per week (plus bills)</strong></p>
-
-				<ul>
-					<li>Fully renovated in 2006</li>
-					<li>4 good-sized bedrooms all including desks and storage facilities</li>
-					<li>One bedroom with ensuite bathroom, one bedroom with ensuite toilet</li>
-					<li>Fully centrally heated</li>
-					<li>uPVC double glazed windows and doors</li>
-					<li>House bathroom with shower, toilet and sink</li>
-					<li>Lounge with dining facilities</li>
-					<li>Fully fitted kitchen with cooker, fridge/freezer, tumble dryer and washing machine</li>
-					<li>Back yard for storage of bikes</li>
-					<li>Free parking on street at front of house</li>
-				</ul>
-<?php
-	require('../../.include/footer.php');
-?>
+header('Location: http://maps.google.co.uk/maps?f=q&hl=en&q=53.945294,-1.092094+(88+Queen+Victoria+St)&ie=UTF8&om=1&ll=53.945295,-1.092099&spn=0.004098,0.010042&z=17&iwloc=addr');
